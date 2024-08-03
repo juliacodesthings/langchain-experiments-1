193afeb90bac974e605b97c84d7b6da1b4bc5d70
juliacodesthings <hello@julia-anderson.com>
2024-08-02 17:34:56 -0700

    bot stuff updated

--- openai-functions/bots/assistants_quickstart.py
+++ openai-functions/bots/assistants_quickstart.py
@@ -16,28 +16,46 @@ def upload_file(path):
     return file 
 
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "What's the check-in time?"}],
-    
-)
+file = upload_file("/Users/juliaanderson/langchain-experiments/data/airbnb-faq.pdf")
 
-print(completion.choices[0].message)
+
+#CREATE ASSISTANT#
+
+# completion = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[{"role": "user", "content": "What's the check-in time?"}],
+    
+# )
+
+# # print(completion.choices[0].message)
 
 def create_assistant(file):
     """
-    You currently canot set the temperature for Assistant via the API.
+    You currently cannot set the temperature for Assistant via the API.
     """
     assistant = client.beta.assistants.create(
         name = "WhatsApp Airbnb Assistant",
-        instructions= "You're a helpflu WhatsApp assistant that can assist guests that are staying in airbnbs",
-        tools=[{"type": "retrieval"}],
+        instructions= "You're a helpful WhatsApp assistant that can assist guests that are staying in airbnbs",
+        tools=[{"type":"file_search",}, {"type": "code_interpreter",}],
         model="gpt-4-1106-preview",
-        file_ids=[file.id],
+       # file_ids=[file.id],
     )
     return assistant
 
-file = upload_file("../data/airbnb-faq.pdf")
 
-file.id
+assistant = create_assistant(file)
 
+#create thread and add message
+
+def generate_response(message_body):
+    thread = client.beta.threads.create()
+    thread_id = thread.id
+    
+    #add message to thread
+    message = client.beta.threads.messages.create(
+        thread_id = thread_id,
+        role="user",
+        content=message_body,
+    )
+    
+message_body = "What's the check-in time?"
