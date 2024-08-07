f5afc6bc9046b5b607878f7519729b1058a8ca1c
juliacodesthings <hello@julia-anderson.com>
2024-08-07 15:56:35 -0700

    assistants bot

--- openai-functions/bots/assistants_quickstart.py
+++ openai-functions/bots/assistants_quickstart.py
@@ -1,48 +1,58 @@
-from openai import OpenAI
-import shelve
-from dotenv import load_dotenv
 import os
+import shelve
 import time
+
+from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 client = OpenAI(api_key=OPEN_AI_API_KEY)
 
 
-#-------
+# -------
 # UPLOAD FILE
-#---------
+# ---------
+
 
 def upload_file(path):
-    #Upload a file with an "assistants" purpose
-    file = client.files.create(file=open(path, "rb"), purpose = "assistants")
-    return file 
+    # Upload a file with an "assistants" purpose
+    file = client.files.create(file=open(path, "rb"), purpose="assistants")
+    return file
 
 
 file = upload_file("/Users/juliaanderson/langchain-experiments/data/airbnb-faq.pdf")
 
-#-------
+# -------
 # CREATE ASSISTANT
 # -------
+
 
 def create_assistant(file):
     """
     You currently cannot set the temperature for Assistant via the API.
     """
     assistant = client.beta.assistants.create(
-        name = "WhatsApp Airbnb Assistant",
-        instructions= "You're a helpful WhatsApp assistant that can assist guests that are staying in our Paris AirBnb. Use your knowledge base to best respond to customer queries. If you don't know the answer, say simply that you cannot help with question and advice to contact the host directly. Be friendly and funny.",
-        tools=[{"type":"file_search",}, {"type": "code_interpreter",}],
-       # tools=[{"type": "retrieval"}],
+        name="WhatsApp Airbnb Assistant",
+        instructions="You're a helpful WhatsApp assistant that can assist guests that are staying in our Paris AirBnb. Use your knowledge base to best respond to customer queries. If you don't know the answer, say simply that you cannot help with question and advice to contact the host directly. Be friendly and funny.",
+        tools=[
+            {
+                "type": "file_search",
+            },
+            {
+                "type": "code_interpreter",
+            },
+        ],
+        # tools=[{"type": "retrieval"}],
         model="gpt-4-1106-preview",
-        #file_ids=[file.id],
-        tool_resources ={ 
-               "code_interpreter": {
-                  "file_ids": [file.id],
-             }
-        }
+        # file_ids=[file.id],
+        tool_resources={
+            "code_interpreter": {
+                "file_ids": [file.id],
+            }
+        },
     )
-    return assistant 
+    return assistant
 
 
 assistant = create_assistant(file)
@@ -52,9 +62,11 @@ assistant = create_assistant(file)
 # Thread management
 # --------------------------------------------------------------
 
+
 def check_if_thread_exists(wa_id):
     with shelve.open("threads_db") as threads_shelf:
         return threads_shelf.get(wa_id, None)
+
 
 def store_thread(wa_id, thread_id):
     with shelve.open("threads_db", writeback=True) as threads_shelf:
@@ -67,7 +79,7 @@ def store_thread(wa_id, thread_id):
 def generate_response(message_body, wa_id, name):
     # Check if there is already a thread_id for the wa_id
     thread_id = check_if_thread_exists(wa_id)
-    
+
     # If a thread doesn't exist, create one and store it
     if thread_id is None:
         print(f"Creating new thread for {name} with wa_id {wa_id}")
@@ -92,52 +104,53 @@ def generate_response(message_body, wa_id, name):
     print(f"To {name}:", new_message)
     return new_message
 
-#create thread and add message
+
+# create thread and add message
 
 
-    # Check if there is already a thread_id for the wa_id
+# Check if there is already a thread_id for the wa_id
 
 #     thread = client.beta.threads.create()
 #     thread_id = thread.id
-       
+
 #         #Add message to thread
 #     message = client.beta.threads.create(
 #             thread_id=thread.id,
 #             role="user",
 #             content=message_body,
 #     )
-        
-# message_body = "What's the check-in time?"
-    
 
-#---------------------
+# message_body = "What's the check-in time?"
+
+
+# ---------------------
 # Run assistant
 # -------------
+
 
 def run_assistant():
     # Retrieve the Assistant
     assistant = client.beta.assistants.retrieve("asst_ifBu0R8DbBDHyFB68Alg4vax")
     thread = client.beta.threads.retrieve("thread_iE9LLVwzWZCwiOUqAdgdTAMF")
-    
+
     # Run the Assistant
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
         assistant_id=assistant.id,
     )
-    
+
     # Wait for completion
-    # https://platform.openai.com/docs/assistants/deep-dive/runs-and-run-steps 
+    # https://platform.openai.com/docs/assistants/deep-dive/runs-and-run-steps
     while run.status != "completed":
-        #Be nice to the API
+        # Be nice to the API
         time.sleep(0.5)
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-        
-    
-    #Retrieve the Messages
+
+    # Retrieve the Messages
     messages = client.beta.threads.messages.list(thread_id=thread.id)
+
     new_message = messages.data[0].content[0].text.value
     return new_message
-
 
 
 # --------------------------------------------------------------
@@ -145,7 +158,6 @@ def run_assistant():
 # --------------------------------------------------------------
 
 new_message = generate_response("What's the check in time?", "123", "John")
-
 new_message = generate_response("What's the pin for the lockbox?", "456", "Sarah")
 
 new_message = generate_response("What was my previous question?", "123", "John")
