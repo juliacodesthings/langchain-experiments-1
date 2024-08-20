a7b7054dc8b08f02c970de4a8fcfd9d09d4ffe58
juliacodesthings <hello@julia-anderson.com>
2024-08-20 15:30:04 -0700

    airbnb bot updates

--- openai-functions/bots/assistants_quickstart.py
+++ openai-functions/bots/assistants_quickstart.py
@@ -13,8 +13,6 @@ client = OpenAI(api_key=OPEN_AI_API_KEY)
 # -------
 # UPLOAD FILE
 # ---------
-
-
 def upload_file(path):
     # Upload a file with an "assistants" purpose
     file = client.files.create(file=open(path, "rb"), purpose="assistants")
@@ -23,29 +21,25 @@ def upload_file(path):
 
 file = upload_file("/Users/juliaanderson/langchain-experiments/data/airbnb-faq.pdf")
 
+
 # -------
 # CREATE ASSISTANT
 # -------
-
-
 def create_assistant(file):
     """
     You currently cannot set the temperature for Assistant via the API.
     """
     assistant = client.beta.assistants.create(
-        name="WhatsApp Airbnb Assistant",
+        name="WhatsApp Airbnb Assistant 2",
         instructions="You're a helpful WhatsApp assistant that can assist guests that are staying in our Paris AirBnb. Use your knowledge base to best respond to customer queries. If you don't know the answer, say simply that you cannot help with question and advice to contact the host directly. Be friendly and funny.",
         tools=[
             {
-                "type": "file_search",
-            },
-            {
                 "type": "code_interpreter",
             },
+            {"type": "file_search"},
         ],
-        # tools=[{"type": "retrieval"}],
         model="gpt-4-1106-preview",
-        # file_ids=[file.id],
+        # file_id=file.id,
         tool_resources={
             "code_interpreter": {
                 "file_ids": [file.id],
@@ -56,7 +50,6 @@ def create_assistant(file):
 
 
 assistant = create_assistant(file)
-
 
 # --------------------------------------------------------------
 # Thread management
@@ -76,51 +69,35 @@ def store_thread(wa_id, thread_id):
 # --------------------------------------------------------------
 # Generate response
 # --------------------------------------------------------------
+
+
 def generate_response(message_body, wa_id, name):
     # Check if there is already a thread_id for the wa_id
     thread_id = check_if_thread_exists(wa_id)
 
-    # If a thread doesn't exist, create one and store it
+    # if a thread doesn't exist, create one and store it
     if thread_id is None:
-        print(f"Creating new thread for {name} with wa_id {wa_id}")
+        print(f"Creating a new thread for {name} with wa_id {wa_id}")
         thread = client.beta.threads.create()
         store_thread(wa_id, thread.id)
         thread_id = thread.id
 
-    # Otherwise, retrieve the existing thread
+    #  Otherwise, add the message to the existing thread
     else:
         print(f"Retrieving existing thread for {name} with wa_id {wa_id}")
         thread = client.beta.threads.retrieve(thread_id)
 
     # Add message to thread
-    message = client.beta.threads.messages.create(
+    message = client.beta.messages.create(
         thread_id=thread_id,
         role="user",
         content=message_body,
     )
 
-    # Run the assistant and get the new message
-    new_message = run_assistant()
+    # Run the assistant and get new message
+    new_message = run_assistant(thread)
     print(f"To {name}:", new_message)
     return new_message
-
-
-# create thread and add message
-
-
-# Check if there is already a thread_id for the wa_id
-
-#     thread = client.beta.threads.create()
-#     thread_id = thread.id
-
-#         #Add message to thread
-#     message = client.beta.threads.create(
-#             thread_id=thread.id,
-#             role="user",
-#             content=message_body,
-#     )
-
-# message_body = "What's the check-in time?"
 
 
 # ---------------------
@@ -131,7 +108,7 @@ def generate_response(message_body, wa_id, name):
 def run_assistant():
     # Retrieve the Assistant
     assistant = client.beta.assistants.retrieve("asst_ifBu0R8DbBDHyFB68Alg4vax")
-    thread = client.beta.threads.retrieve("thread_iE9LLVwzWZCwiOUqAdgdTAMF")
+    # thread = client.beta.threads.retrieve("thread_iE9LLVwzWZCwiOUqAdgdTAMF")
 
     # Run the Assistant
     run = client.beta.threads.runs.create(
@@ -148,8 +125,8 @@ def run_assistant():
 
     # Retrieve the Messages
     messages = client.beta.threads.messages.list(thread_id=thread.id)
-
     new_message = messages.data[0].content[0].text.value
+    print(f"Generated message: {new_message}")
     return new_message
 
 
